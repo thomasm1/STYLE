16bc2e524571cb8a540469e298bdc2c52fc8013d
thomas maestas <thomasm1.maestas@gmail.com>
2019-08-13 21:34:12 -0600

    IntersectionObserver function for lazy-loading imgs

---
+++ lazy-load/lazy.js
@@ -0,0 +1,21 @@
+const targets = document.querySelectorAll('img');
+
+const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            console.log('image-load!!'); 
+
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                const src = img.getAttribute('data-lazy');
+
+                img.setAttribute('src', src);
+                img.classList.add('fade');
+                
+                observer.disconnect();
+            } 
+        });
+    });
+    io.observe(target);
+}
+targets.forEach(lazyLoad)
